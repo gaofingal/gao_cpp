94bd5f6ce35dab42371b897f430885cef6119b5f
gao <longyuan7@qq.com>
2024-04-12 23:38:32 +0800

    maxprices

--- gao_letcode/SolutionA.cpp
+++ gao_letcode/SolutionA.cpp
@@ -1,45 +1,51 @@
-#include"SolutionA.h"
-#include<algorithm>
-#include<iostream>
+#include "SolutionA.h"
+#include <algorithm>
+#include <iostream>
 
-void SolutionA::merge1(std::vector<int>& num1, int m, std::vector<int>& num2, int n)
-     {
-     while(m > 0 && n > 0){
-         if(num1[m - 1] < num2[n - 1]){
-             num1[m + n - 1] = num2[n - 1];
-             --n;
-         }else{
-             num1[m + n -1] = num1[m - 1];
-             --m;
-         }
-        }
-        if (m == 0 && n > 0){
-         for (int i = 0; i < n; ++i){
-             num1[i] = num2[i];
-         }
-        }
-     };
-
-void SolutionA::merge2(std::vector<int>& num1, int m, std::vector<int>& num2, int n)
+void SolutionA::merge1(std::vector<int> &num1, int m, std::vector<int> &num2, int n)
 {
-    int len = m + n -1;
+    while (m > 0 && n > 0)
+    {
+        if (num1[m - 1] < num2[n - 1])
+        {
+            num1[m + n - 1] = num2[n - 1];
+            --n;
+        }
+        else
+        {
+            num1[m + n - 1] = num1[m - 1];
+            --m;
+        }
+    }
+    if (m == 0 && n > 0)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            num1[i] = num2[i];
+        }
+    }
+};
+
+void SolutionA::merge2(std::vector<int> &num1, int m, std::vector<int> &num2, int n)
+{
+    int len = m + n - 1;
     --m, --n;
-    while (m>-1 && n>-1)
+    while (m > -1 && n > -1)
     {
         num1[len--] = num1[m] > num2[n] ? num1[m--] : num2[n--];
     }
     while (n > -1)
     {
-        num1[len--]= num2[n--];
+        num1[len--] = num2[n--];
     }
-    
 };
 
-int removeDup(std::vector<int>& nums){
-    
+int SolutionA::removeDup(std::vector<int> &nums)
+{
+
     std::sort(nums.begin(), nums.end());
 
-    if(nums.size() < 2)
+    if (nums.size() < 2)
     {
         return nums.size();
     }
@@ -51,85 +57,142 @@ int removeDup(std::vector<int>& nums){
         {
             nums[++k] = nums[i];
         }
-        
     }
     return ++k;
 };
-    int removeElement(std::vector<int>& nums, int val){
-        int k = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] != val)
-            {
-                // 1 3 9 3 7 20 16
-                nums[k] = nums[i];
-                ++k;
-            }
-        }
-        
-    };
-int removeDuplicates(std::vector<int>& nums){
-        // 1,1,1,1,2,3,4,4,4,5,6,7
-        int k = 0;
-        int d = 0;
-        for(int i = 1; i < nums.size(); ++i)
-        {
-            if(nums[k] != nums[i])
-            {
-                d = 0;
-                ++k;
-                nums[k] = nums[i];
-            }
-            else
-            {
-                ++d;
-                if(d == 1)
-                {
-                    k =k + 1;
-                    nums[k] = nums[i];
-                }
-            }
-        }
-        return ++k;
-    };
-     int majorityElement(std::vector<int>& nums)
+
+int SolutionA::removeElement(std::vector<int> &nums, int val)
+{
+    int k = 0;
+    for (int i = 0; i < nums.size(); i++)
     {
-        sort(nums.begin(), nums.end());
-        int majorityEle = 0;
-        int k = 0;
-        int count = 0;
-        for (int i = 1; i < nums.size(); i++)
+        if (nums[i] != val)
         {
-            if(nums[k] != nums[i])
-            {
-                if ((i - k) > count)
-                {
-                    count = i - k;
-                    majorityEle = nums[k];
-                }
-                k = i; 
-            }
-            if (nums.size() == i + 1)
-            {
-                if ((nums[i] == nums[k]) && ((i - k + 1) > count))
-                {
-                    majorityEle = nums[k];
-                }  
-            }
-            
-            
+            // 1 3 9 3 7 20 16
+            nums[k] = nums[i];
+            ++k;
         }
-        return majorityEle;   
-    };
+    }
+};
+int SolutionA::removeDuplicates(std::vector<int> &nums)
+{
+    // 1,1,1,1,2,3,4,4,4,5,6,7
+    int k = 0;
+    int d = 0;
+    for (int i = 1; i < nums.size(); ++i)
+    {
+        if (nums[k] != nums[i])
+        {
+            d = 0;
+            ++k;
+            nums[k] = nums[i];
+        }
+        else
+        {
+            ++d;
+            if (d == 1)
+            {
+                k = k + 1;
+                nums[k] = nums[i];
+            }
+        }
+    }
+    return ++k;
+};
 
-void rotate(std::vector<int>& nums, int k){
-        k = k%nums.size();
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin()+k, nums.end());
-    };
+int SolutionA::majorityElement(std::vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    int majorityEle = 0;
+    int k = 0;
+    int count = 0;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[k] != nums[i])
+        {
+            if ((i - k) > count)
+            {
+                count = i - k;
+                majorityEle = nums[k];
+            }
+            k = i;
+        }
+        if (nums.size() == i + 1)
+        {
+            if ((nums[i] == nums[k]) && ((i - k + 1) > count))
+            {
+                majorityEle = nums[k];
+            }
+        }
+    }
+    return majorityEle;
+};
 
+void SolutionA::rotate(std::vector<int> &nums, int k)
+{
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+};
 
-void sayHi(){
-    std::cout<<"hi"<<std::endl;
+int SolutionA::maxProfit(std::vector<int> &prices)
+{
+    int maxpro = 0;
+    for (int i = 0; i < prices.size(); i++)
+    {
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            if (prices[i] < prices[j])
+            {
+                if (maxpro < prices[j] - prices[i])
+                {
+                    maxpro = prices[j] - prices[i];
+                }
+            }
+        }
+    }
+    return maxpro;
+};
+ int SolutionA::maxProfitV1(std::vector<int>& prices)
+ {
+      int low = INT_MAX;
+        int result = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            low = std::min(low, prices[i]);  // 取最左最小价格
+            result = std::max(result, prices[i] - low); // 直接取最大区间利润
+        }
+        return result;
+ };
+
+ int SolutionA::maxProfitV2(std::vector<int>& prices)
+ {
+    int len = prices.size();
+        if (len == 0) return 0;
+        std::vector<std::vector<int>> dp(len, std::vector<int>(2));
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = std::max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = std::max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+        }
+        return dp[len - 1][1];
+ };
+
+ int SolutionA::maxProfitV3(std::vector<int>& prices)
+ {
+ int len = prices.size();
+        std::vector<std::vector<int>> dp(2, std::vector<int>(2)); // 注意这里只开辟了一个2 * 2大小的二维数组
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < len; i++) {
+            dp[i % 2][0] = std::max(dp[(i - 1) % 2][0], -prices[i]);
+            dp[i % 2][1] = std::max(dp[(i - 1) % 2][1], prices[i] + dp[(i - 1) % 2][0]);
+        }
+        return dp[(len - 1) % 2][1];
+ };
+
+void SolutionA::sayHi()
+{
+    std::cout << "hi" << std::endl;
 };
--- gao_letcode/SolutionA.h
+++ gao_letcode/SolutionA.h
@@ -10,10 +10,34 @@ public:
     */
     void merge1(std::vector<int>& num1, int m, std::vector<int>& num2, int n);
     void merge2(std::vector<int>& num1, int m, std::vector<int>& num2, int n);
+    /**
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     */
     int removeDup(std::vector<int>& nums);
+    /**
+     * 给你一个 非严格递增排列 的数组 nums ，请你原地删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+     * 然后返回 nums 中唯一元素的个数。
+     */
     int removeElement(std::vector<int>& nums, int m);
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+     */
     int removeDuplicates(std::vector<int>& nums);
+    /**
+     * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     */
     int majorityElement(std::vector<int>& nums);
+    /**
+     * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+     */
     void rotate(std::vector<int>& nums, int k);
+    /**
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     */
+    int maxProfit(std::vector<int>& prices);
     void sayHi();
 };
